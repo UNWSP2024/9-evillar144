c6dbe19de538cc3fd32376ee1e64c9e8791571d1
evillar144 <elivillar144@gmail.com>
2025-03-28 13:36:41 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,5 +1,16 @@
 # Program #2: Random Number File Writer
 # Write a program that writes a series of random numbers (up to 1000) to a file.
-# Each random number should be in the range of 1 through 500. 
-# The application should let the user specify how many random numbers the file will hold 
+# Each random number should be in the range of 1 through 500.
+# The application should let the user specify how many random numbers the file will hold
 # (up to 1000).
+
+import random
+question = int(input("How many random numbers do you want (up to 1000): "))
+if question > 1000:
+    print("YOU CANT DO THAT DUMBASS")
+else:
+    with open('text.txt', 'w') as file:
+        for _ in range(question):
+            num = random.randint(1, 500)
+            file.write(f"{num}\n")
+        print(f"{question} random numbers have been written to 'text.txt'.")
